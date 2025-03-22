4154a0639e785146601900f167b66f32776cb057
phammquan <phamminhq91@gmail.com>
2025-03-23 00:05:58 +0700

    done UI home, add reminder, register

---
+++ app/src/main/java/com/example/calendar_app/Activities/AddReminder.java
@@ -0,0 +1,15 @@
+package com.example.calendar_app.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.calendar_app.R;
+
+public class AddReminder extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reminder);
+    }
+}
